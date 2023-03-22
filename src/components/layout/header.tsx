05b49fbb63949ff13505ae90033873f400bba80d
Nithin-Varma <mnithin1422@gmail.com>
2023-03-22 22:43:24 +0530

    initial commit

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,21 @@
+// import LanguagesButton from '@/components/languages-button';
+import ThemeButton from '@/components/theme-button';
+import { HStack } from '@chakra-ui/react';
+
+const Header = () => {
+  return (
+    <HStack
+      as='header'
+      position='fixed'
+      top='0'
+      p={8}
+      zIndex='tooltip'
+      justify='space-between'
+      align='center'
+      w='100%'
+    >
+    </HStack>
+  );
+};
+
+export default Header;
