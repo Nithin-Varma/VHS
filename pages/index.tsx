31d26a868f5c7870f0fc9a0aeaf9a9182655c222
Nithin-Varma <mnithin1422@gmail.com>
2023-03-30 17:37:36 +0530

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,64 @@
 import {
-  Flex,
-  Container,
-  Heading,
   Stack,
-  Text,
+  Flex,
   Button,
-  Icon,
-  IconProps,
+  Text,
   VStack,
   useBreakpointValue,
-} from '@chakra-ui/react';
+  SimpleGrid,
+} from "@chakra-ui/react";
 
-export default function Home() {
+export default function LandingView() {
   return (
-   <>
     <Flex
-      w={'full'}
-      h={'70vh'}
-      
-      backgroundImage={
-        'url(/assets/images/VHS.jpeg)'
-      }
-      backgroundSize={'cover'}
-      backgroundPosition={'center center'}
-      >
+      backgroundImage="url(/assets/images/VHS.jpeg)"
+      backgroundSize="100%"
+      height={{ base: "60vh", md: "80vh" }}
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      _hover={{
+        transform: "scale(0.98)",
+        borderColor: "#6C0BA9",
+        // filter: "blur(20px)",
+        // shadow: "2px 4px 5px #6C0BA9, 2px 4px 5px #6C0BA9",
+        boxShadow: "0px 1px 10px 2px #1E90FF, 0 4px 4px 2px #1E90FF",
+      }}
+    >
       <VStack
-        marginTop={'80'}
-        w={'full'}
-        justify={'center'}
-        
+        w="full"
+        justify="center"
         px={useBreakpointValue({ base: 4, md: 8 })}
-        // bgGradient={'linear(to-r, blackAlpha.600, transparent)'}
+        bgGradient="linear(to-r, blackAlpha.600, transparent)"
+      >
+        <VStack
+          maxW={useBreakpointValue({ base: "2xl", md: "4xl" })}
+          align="left"
+          marginTop="80"
         >
-        <Stack  align={'flex-start'} spacing={6}>
           <Text
-            color={'white'}
-            fontWeight={700}
-            lineHeight={1.2}
-            dropShadow={'dark-lg'}
-            textShadow='9px 5px 10px black' m='32'
-            fontSize={useBreakpointValue({ base: '5xl', md: '6xl' })}>
-            We don't Teach,{' '} 
-            <br/>
-            <Text as={'span'} color={'blue.500'} >We Educate</Text>
+            color="whiteAlpha.800"
+            fontWeight="extrabold"
+            lineHeight={2.2}
+            align="center"
+            fontFamily="sans-serif"
+            textShadow="6px 3px 9px #6C0BA9"
+            fontSize={useBreakpointValue({ base: "3xl", md: "6xl" })}
+          >
+            We Don't Teach
           </Text>
-        </Stack>
+          <Text
+            color="whiteAlpha.800"
+            fontWeight="extrabold"
+            lineHeight={2.2}
+            align="center"
+            fontFamily="sans-serif"
+            textShadow="6px 3px 9px #6C0BA9"
+            fontSize={useBreakpointValue({ base: "2xl", md: "5xl" })}
+          >
+            We Educate
+          </Text>
+        </VStack>
       </VStack>
     </Flex>
-   </>
   );
 }
